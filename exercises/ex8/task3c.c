8395ae597ad6f0db4304ffba723e4341d698dfcb
ameetmadan <ameetmadan@gmail.com>
2021-04-25 19:22:20 +0200

    ex08 updated

--- exercises/ex8/task3c.c
+++ exercises/ex8/task3c.c
@@ -1,14 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct TreeNode
+struct Node *InPre(struct Node *p) /* how do we handle the nodes after deletion */
 {
-    int val;
-    int data;
-    struct TreeNode *left;
-    struct TreeNode *right;
-};
+    while (p && p->rchild != NULL)
+        p = p->rchild;
+    return p;
+}
 
-void delete (struct TreeNode **root, int val)
+struct Node *InSucc(struct Node *p) /* how do we handle the nodes after deletion */
 {
+    while (p && p->lchild != NULL)
+        p = p->lchild;
+    return p;
+}
+
+struct Node *Delete(struct Node *p, int key)
+{
+    struct Node *q;
+    if (p == NULL)
+        return NULL;
+    if (p->lchild == NULL && p->rchild == NULL)
+    {
+        if (p == root)
+            root = NULL;
+        free(p);
+        return NULL;
+    }
+    if (key < p->data)
+        p->lchild = Delete(p->lchild, key);
+    else if (key > p->data)
+        p->rchild = Delete(p->rchild, key);
+    else
+    {
+        if (Height(p->lchild) > Height(p->rchild))
+        {
+            q = InPre(p->lchild);
+            p->data = q->data;
+            p->lchild = Delete(p->lchild, q->data);
+        }
+        else
+        {
+            q = InSucc(p->rchild);
+            p->data = q->data;
+            p->rchild = Delete(p->rchild, q->data);
+        }
+    }
+    return p;
 }
--- exercises/ex8/task3g.c
+++ exercises/ex8/task3g.c
@@ -1,3 +1,12 @@
 int distanceToRoot(struct TreeNode *root, int val)
 {
 }
+/* 
+int Height(struct Node *p)
+{
+int x,y;
+if(p==NULL)return 0;
+x=Height(p->lchild);
+y=Height(p->rchild);
+return x>y?x+1:y+1;
+} */
