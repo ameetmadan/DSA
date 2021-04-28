21c033ec8c8af941dc14354ed20c121bc610c19a
ameetmadan <ameetmadan@gmail.com>
2021-04-28 19:34:23 +0200

    coding in C added

---
+++ code/coding_in_C.c
@@ -0,0 +1,19 @@
+/* Running C code */
+
+/* Printing and scanning from console */
+
+/* Handling strings */
+
+/* Create and handle arrays */
+
+/* Operations on arrays */
+
+/* Conditional flows (if, else, elif) */
+
+/* For loops and variants */
+
+/* While loops */
+
+/* Writing and calling functions */
+
+/* Pointers and addresses */
