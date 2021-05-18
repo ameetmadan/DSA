3dddcbf0c94eb45607ffc4d0fb0ddcd47a2be6b6
ameetmadan <ameetmadan@gmail.com>
2021-05-18 15:11:05 +0200

    DS adde

--- code/data_structures/stacks.c
+++ code/data_structures/stacks.c
@@ -1,39 +1,102 @@
-/* Copied from https://github.com/JeromeHadorn/UZH_CS_Bachelor_Neuroinformatics/ */
+// Stack implementation in C
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int A[10];
-int t = 0;
+#define MAX 10
 
-void printStack()
+int count = 0;
+
+// Creating a stack
+struct stack
 {
-    for (int i = 0; i < t; i++)
+    int items[MAX];
+    int top;
+};
+typedef struct stack st;
+
+void createEmptyStack(st *s)
+{
+    s->top = -1;
+}
+
+// Check if the stack is full
+int isfull(st *s)
+{
+    if (s->top == MAX - 1)
+        return 1;
+    else
+        return 0;
+}
+
+// Check if the stack is empty
+int isempty(st *s)
+{
+    if (s->top == -1)
+        return 1;
+    else
+        return 0;
+}
+
+// Add elements into stack
+void push(st *s, int newitem)
+{
+    if (isfull(s))
     {
-        printf("%d,", A[i]);
+        printf("STACK FULL");
     }
+    else
+    {
+        s->top++;
+        s->items[s->top] = newitem;
+    }
+    count++;
 }
 
-void push(int x)
+// Remove element from stack
+void pop(st *s)
 {
-    A[t] = x;
-    t++;
+    if (isempty(s))
+    {
+        printf("\n STACK EMPTY \n");
+    }
+    else
+    {
+        printf("Item popped= %d", s->items[s->top]);
+        s->top--;
+    }
+    count--;
+    printf("\n");
 }
 
-int pop()
+// Print elements of stack
+void printStack(st *s)
 {
-    t--;
-    return A[t];
+    printf("Stack: ");
+    for (int i = 0; i < count; i++)
+    {
+        printf("%d ", s->items[i]);
+    }
+    printf("\n");
 }
 
+// Driver code
 int main()
 {
+    int ch;
+    st *s = (st *)malloc(sizeof(st));
 
-    push(10);
-    printStack();
-    push(20);
-    printStack();
-    pop();
-    printStack();
-    return 0;
+    createEmptyStack(s);
+
+    push(s, 1);
+    push(s, 2);
+    push(s, 3);
+    push(s, 4);
+
+    printStack(s);
+
+    pop(s);
+
+    printf("\nAfter popping out\n");
+    printStack(s);
 }
